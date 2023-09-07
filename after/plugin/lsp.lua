8a8dc560b911b64261b441d19118d426e116575f
Luciano Correia <luciano.correia@gmail.com>
2023-09-07 12:46:50 +0400

    update config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -80,7 +80,43 @@ rust_tools.setup({
         on_attach = function(_, bufnr)
             vim.keymap.set('n', '<leader>.', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
         end
-    }
+    },
+    -- These apply to the default RustSetInlayHints command
+    inlay_hints = {
+        -- automatically set inlay hints (type hints)
+        -- default: true
+        auto = true,
+
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+
+        -- whether to show parameter hints with the inlay hints or not
+        -- default: true
+        show_parameter_hints = true,
+
+        -- prefix for parameter hints
+        -- default: "<-"
+        parameter_hints_prefix = "<- ",
+
+        -- prefix for all the other hints (type, chaining)
+        -- default: "=>"
+        other_hints_prefix = "=> ",
+
+        -- whether to align to the length of the longest line in the file
+        max_len_align = false,
+
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+
+        -- whether to align to the extreme right or not
+        right_align = true,
+
+        -- padding from the right if right_align is true
+        right_align_padding = 5,
+
+        -- The color of the hints
+        highlight = "Comment",
+    },
 })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
@@ -204,4 +240,3 @@ require("mason-null-ls").setup({
 })
 
 require("inlay-hints").setup()
-

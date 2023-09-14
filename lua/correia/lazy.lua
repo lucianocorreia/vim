eba65b3e90a2760ac5a44a0925683dd300a34f31
Luciano Correia <luciano.correia@gmail.com>
2023-09-14 09:48:56 +0400

    update config

--- lua/correia/lazy.lua
+++ lua/correia/lazy.lua
@@ -17,7 +17,14 @@ require("lazy").setup({
 
     -- Colorscheme
     { "catppuccin/nvim",       name = "catppuccin", priority = 1000 },
-
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({})
+        end,
+    },
     -- Detect tabstop and shiftwidth automatically
     -- { 'tpope/vim-sleuth' },
 

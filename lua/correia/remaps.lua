4c2cf77a42b685462675a366838d7c5c7c15fcbc
Luciano Correia <luciano.correia@gmail.com>
2023-09-20 20:33:53 +0400

    update config

--- lua/correia/remaps.lua
+++ lua/correia/remaps.lua
@@ -83,14 +83,14 @@ vnoremap(">", ">gv", silent)
 nnoremap("<leader>h", ":nohl<CR>", silent)
 
 -- wrap
-nnoremap("<leader>wa", "<Cmd>set wrap<CR>", silent)
-nnoremap("<leader>wq", "<cmd>set nowrap<cr>", silent)
+-- nnoremap("<leader>wq", "<Cmd>set wrap<CR>", silent)
+-- nnoremap("<leader>wq", "<cmd>set nowrap<cr>", silent)
 
 -- misc
 -- vnoremap("<leader>p", "\"_dp")
 -- vnoremap("<leader>P", "\"_dP")
 nnoremap("<leader>rp", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-nnoremap("<leader>ww", "<Cmd>w<CR>")
+nnoremap("<leader>w", "<Cmd>w<CR>")
 -- nnoremap("<leader>q", "<Cmd>q<CR>")
 nnoremap("<leader><C-o>", "<Cmd>!open %<CR><CR>", silent)
 -- nnoremap("J", "mzJ`z")

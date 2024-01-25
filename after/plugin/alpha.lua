8ad09e67dda08077d2d6eaff8cdcc4808123626d
Luciano Correia <luciano.correia@gmail.com>
2024-01-25 10:33:30 +0400

    update config

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -1,1 +1,15 @@
-require 'alpha'.setup(require 'alpha.themes.startify'.config)
+-- require 'alpha'.setup(require 'alpha.themes.startify'.config)
+-- require('alpha').setup(function()
+--     local alpha = require("alpha")
+--     local startify = require("alpha.themes.startify")
+
+--     startify.section.header.val = {
+--         [[             _             _               ]],
+--         [[        ___ | | _ __ ___  (_) _   _  _ __  ]],
+--         [[       / _ \| || '_ ` _ \ | || | | || '__| ]],
+--         [[      |  __/| || | | | | || || |_| || |    ]],
+--         [[       \___||_||_| |_| |_||_| \__,_||_|    ]],
+--     }
+
+--      alpha.setup(startify.config)
+-- end)
--- lua/correia/lazy.lua
+++ lua/correia/lazy.lua
@@ -162,6 +162,39 @@ require("lazy").setup({
     {
         'goolord/alpha-nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            local alpha = require("alpha")
+            local startify = require("alpha.themes.startify")
+
+            -- startify.section.header.val = {
+            --     [[      ::::::::   ::::::::  :::::::::  :::::::::  :::::::::: :::::::::::     :::   ]],
+            --     [[    :+:    :+: :+:    :+: :+:    :+: :+:    :+: :+:            :+:       :+: :+:  ]],
+            --     [[   +:+        +:+    +:+ +:+    +:+ +:+    +:+ +:+            +:+      +:+   +:+  ]],
+            --     [[  +#+        +#+    +:+ +#++:++#:  +#++:++#:  +#++:++#       +#+     +#++:++#++:  ]],
+            --     [[ +#+        +#+    +#+ +#+    +#+ +#+    +#+ +#+            +#+     +#+     +#+   ]],
+            --     [[#+#    #+# #+#    #+# #+#    #+# #+#    #+# #+#            #+#     #+#     #+#    ]],
+            --     [[########   ########  ###    ### ###    ### ########## ########### ###     ###     ]],
+            -- }
+
+            -- startify.opts.layout[1].val = 2
+            -- startify.opts.opts.margin = 43
+
+            -- -- disable MRU
+            -- startify.section.mru.val = { { type = "padding", val = 0 } }
+
+            -- Set menu
+            startify.section.top_buttons.val = {
+                startify.button("SPC ee", " > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+                startify.button("SPC sf", " > Find File", "<cmd>Telescope find_files<CR>"),
+                startify.button("SPC sw", " > Find Word", "<cmd>Telescope live_grep<CR>"),
+            }
+
+            -- Send config to alpha
+            alpha.setup(startify.config)
+
+            -- Disable folding on alpha buffer
+            vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+        end
     },
 
     -- trouble

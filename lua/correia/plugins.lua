d421348f56209a03d81222a76b51324897d84fa8
Luciano Correia <luciano.correia@gmail.com>
2024-03-15 12:16:42 +0400

    update config

--- lua/correia/plugins.lua
+++ lua/correia/plugins.lua
@@ -218,7 +218,7 @@ require("lazy").setup({
             require("rose-pine").setup({
                 variant = "main",      -- auto, main, moon, or dawn
                 dark_variant = "main", -- main, moon, or dawn
-                dim_inactive_windows = false,
+                dim_inactive_windows = true,
                 extend_background_behind_borders = true,
 
                 enable = {
